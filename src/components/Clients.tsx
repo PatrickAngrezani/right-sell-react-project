fc0a40dbc9e20d69c54263afb19e72bb4296a5bd
PatrickAngrezani <pangrezani@gmail.com>
2024-10-30 18:16:36 -0300

    update clients component

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,5 +1,17 @@
 import React from "react";
-import { Box, Typography, Grid, Paper } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
+
+import antilhasVarejo from "../assets/antilhas-varejo.png";
+import braniva from "../assets/braniva.png";
+import cadernoVirtual from "../assets/caderno-virtual.png";
+import chaProd from "../assets/cha-prod.png";
+import grupoForest from "../assets/grupo-forest.png";
+import jmfConsults from "../assets/jmf-consults.png";
+import marcasPatentes from "../assets/marcas-patentes.png";
+import nityoInfotech from "../assets/nityo-infotech.png";
+import noAge from "../assets/no-age.png";
+import ravim from "../assets/ravim.png";
+import recrutify from "../assets/recrutify.png";
 
 interface Client {
   name: string;
@@ -7,26 +19,50 @@ interface Client {
 }
 
 const clients: Client[] = [
-  { name: "ravim", logo: "" },
-  { name: "recrutify", logo: "" },
+  { name: "antilhas-varejo", logo: antilhasVarejo },
+  { name: "braniva", logo: braniva },
+  { name: "cadernoVirtual", logo: cadernoVirtual },
+  { name: "chaProd", logo: chaProd },
+  { name: "grupoForest", logo: grupoForest },
+  { name: "jmfConsults", logo: jmfConsults },
+  { name: "marcasPatentes", logo: marcasPatentes },
+  { name: "nityoInfotech", logo: nityoInfotech },
+  { name: "noAge", logo: noAge },
+  { name: "ravim", logo: ravim },
+  { name: "recrutify", logo: recrutify },
 ];
 
 const Clients: React.FC = () => {
   return (
-    <Box>
-      <Typography variant="h4" textAlign="center" gutterBottom>
+    <Box sx={{ padding: "4rem 0", backgroundColor: "#007f3b" }}>
+      <Typography
+        variant="h4"
+        textAlign="center"
+        gutterBottom
+        sx={{ color: "#ffffff", fontWeight: "bold", marginBottom: "2rem" }}
+      >
         Nossos Clientes
       </Typography>
-      <Grid container spacing={2} justifyContent="center">
+      <Grid container spacing={3} justifyContent="center">
         {clients.map((client, index) => (
-          <Grid key={index} xs={6} sm={3} md={2}>
-            <Paper elevation={3} sx={{ padding: "1rem", textAlign: "center" }}>
+          <Grid item xs={6} sm={4} md={3} lg={2} key={index}>
+            <Box
+              sx={{
+                padding: "1rem",
+                textAlign: "center",
+                backgroundColor: "#007f3b",
+              }}
+            >
               <img
                 src={client.logo}
                 alt={client.name}
-                style={{ width: "100%" }}
+                style={{
+                  width: "100%",
+                  maxHeight: "80px",
+                  objectFit: "contain",
+                }}
               />
-            </Paper>
+            </Box>
           </Grid>
         ))}
       </Grid>
