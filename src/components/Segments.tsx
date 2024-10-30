ea6bc13950c0d82435d0ae6237343449835baa37
PatrickAngrezani <pangrezani@gmail.com>
2024-10-30 17:26:51 -0300

    update segments component

--- src/components/Segments.tsx
+++ src/components/Segments.tsx
@@ -1,24 +1,52 @@
 import React from "react";
-import { Box, Typography, Button, Grid } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
+
 const segments: string[] = [
-  "Tecnologia",
-  "Consultoria",
-  "Startup",
-  "Indústria",
-  "Marketing",
-  "Outros",
+  "TECNOLOGIA & CONSULTORIA",
+  "STARTUP & INDÚSTRIA",
+  "RECURSOS HUMANOS",
+  "CORRETORAS & SEGURADORAS",
+  "MARKETING",
+  "COMPRAS & OPERAÇÕES",
 ];
 
 const Segments: React.FC = () => {
   return (
-    <Box sx={{ padding: "4rem", textAlign: "center" }}>
-      <Typography variant="h4" gutterBottom>
-        Segmentos de Atuação
+    <Box
+      sx={{
+        padding: "4rem",
+        textAlign: "center",
+        backgroundColor: "#e9ecef",
+        fontFamily: "Roboto",
+      }}
+    >
+      <Typography
+        variant="h4"
+        gutterBottom
+        sx={{ color: "#007f3b", fontWeight: "bold" }}
+      >
+        SEGMENTOS DE ATUAÇÃO
       </Typography>
       <Grid container spacing={2} justifyContent="center">
         {segments.map((segment, index) => (
-          <Grid item key={index}>
-            <Button variant="outlined">{segment}</Button>
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Box
+              sx={{
+                padding: "1rem",
+                fontWeight: "bold",
+                color: "#333",
+                border: "1px solid #ddd",
+                backgroundColor: "#fff",
+                textAlign: "center",
+                borderRadius: "4px",
+                "&:hover": {
+                  backgroundColor: "#f0f0f0",
+                  borderColor: "#ccc",
+                },
+              }}
+            >
+              {segment}
+            </Box>
           </Grid>
         ))}
       </Grid>
