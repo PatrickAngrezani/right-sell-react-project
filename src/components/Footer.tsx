de7ae48666d3f0beee6b4fdd9b3ab031e2758ee2
PatrickAngrezani <pangrezani@gmail.com>
2024-10-31 00:19:46 -0300

    update footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 function Footer() {
   return (
@@ -11,7 +12,26 @@ function Footer() {
         color: "#fff",
       }}
     >
-      <Typography>© 2024 Venda Certo - Todos os direitos reservados</Typography>
+      <Button
+        href="https://www.google.com/url?q=https%3A%2F%2Fwa.me%2F%2F5511959455865%3Ftext%3DVim%2520pelo%2520site%2520%2C%2520tenho%2520interesse%2520em%2520conhecer%2520mais%2520a%2520VendaCerto&sa=D&sntz=1&usg=AOvVaw2ht1pfRew_v7VKF9hm6fdj"
+        target="_blank"
+        variant="contained"
+        startIcon={<WhatsAppIcon />}
+        sx={{
+          backgroundColor: "#007f3b",
+          color: "#fff",
+          textTransform: "none",
+          marginBottom: "1rem",
+          "&:hover": {
+            backgroundColor: "#005a29",
+          },
+        }}
+      >
+        Entre em contato com um de nossos especialistas
+      </Button>
+      <Typography sx={{ marginTop: "1 rem" }}>
+        © 2024 Venda Certo - Todos os direitos reservados
+      </Typography>
     </Box>
   );
 }
