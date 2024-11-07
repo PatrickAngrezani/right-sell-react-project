b9fb82fc465eaa218b4d065a94ea87ff85430556
PatrickAngrezani <pangrezani@gmail.com>
2024-11-07 15:17:56 -0300

    update plans texts component

--- src/components/PlanosPage.tsx
+++ src/components/PlanosPage.tsx
@@ -4,40 +4,40 @@ import { colors } from "../colors.tsx";
 
 const plansData = [
   {
-    title: "PLANO LDR",
+    title: "LDR",
     subtitleColor: "#8B0000",
     items: [
-      "Lead Development Representative (LDR): Profissional treinado pela metodologia Venda Certo à disposição do cliente;",
-      "Copywriter Profissional: Construção de textos de prospecção para e-mail, Whatsapp e LinkedIn;",
-      "Definição de ICPs junto ao cliente;",
-      "Prospecção e qualificação de leads, além de automação de disparos: E-mail, Whatsapp e LinkedIn;",
-      "Enriquecimento de listas de prospecção diariamente: Criamos diretamente o seu banco de leads personalizado, não compramos nem utilizamos listas prontas!",
-      "Metodologia e plataforma de LDR.",
-      "Neste plano, já provamos reuniões pré-qualificadas e 1 (um) profissional LDR.",
+      "Lead Development Representative (LDR): O profissional LDR é treinado na metodologia exclusiva Venda Certo, garantindo a entrega das demandas de maneira eficiente e alinhada às necessidades do cliente;",
+      "Copywriter Profissional: Desenvolve textos técnicos e persuasivos para prospecção por e-mail, WhatsApp e LinkedIn, sempre alinhados com o discurso e a identidade do cliente;",
+      "Definição de ICPs (Ideal Customer Profile): Trabalhamos com o cliente para identificar os nichos de mercado, cargos dos leads e segmentos de empresas a serem abordados, garantindo uma prospecção mais assertiva;",
+      "Prospecção e Enriquecimento de Listas: Em vez de comprar listas frias, criamos e enriquecemos listas de leads do zero, utilizando nossa metodologia e tecnologia para garantir a melhor qualidade. (atualização semanal);",
+      "Automação de Disparos em Cadência: Realizamos disparos de e-mails em massa (100-150 por semana, inclusos), WhatsApp e LinkedIn. Para envios em massa via WhatsApp e LinkedIn, consulte nossos planos especializados. Todos os disparos são feitos com total controle e monitoramento;",
+      "Metodologia e Plataforma Proprietária de LDR: Utilizamos uma plataforma exclusiva e metodologia proprietária para o acompanhamento detalhado das tarefas e evolução dos leads;",
+      "Reuniões Pré Qualificadas: Em nosso plano, já incluímos a realização de reuniões pré-qualificadas, otimizando o processo e garantindo que o cliente receba leads altamente relevantes e preparados para a conversão.",
     ],
   },
   {
-    title: "PLANO BDR",
+    title: "BDR / SDR",
     subtitleColor: "silver",
     items: [
       "Tudo que engloba o plano LDR, além de:",
-      "1 (um) profissional BDR/SDR híbrido e dedicado (inbound e outbound): Profissional treinado pela metodologia Venda Certo à disposição do cliente;",
-      "Sales Development Representative (SDR): Este profissional é responsável por atender e ativar as demandas de prospecção geradas pelo cliente - SDR/inbound;",
-      "Business Development Representative (BDR): Este profissional é responsável por atender e ativar as demandas de prospecção geradas pelo LDR da Venda Certo - BDR/outbound;",
-      "Metodologia de agendamento e qualificação de reuniões: Este profissional é responsável por agendar e qualificar as reuniões comerciais, segundo as metas estipuladas pelo cliente;",
-      "CRM: Este profissional é responsável por operar o pipeline de vendas/CRM do cliente com as demandas inbound e outbound, até a etapa “apresentação” e acompanhar as follow-ups de pré-vendas.",
+      "Profissional BDR/SDR Híbrido e Dedicado (Inbound e Outbound): Este profissional, treinado na metodologia exclusiva Venda Certo, ficará dedicado exclusivamente ao cliente, gerenciando tanto a prospecção inbound quanto outbound com eficiência;",
+      "Sales Development Representative (SDR): Responsável por ativar e atender às demandas de prospecção geradas pelo cliente, trabalhando os leads inbound e qualificando-os para avançar no funil de vendas;",
+      "Business Development Representative (BDR): Focado em atender e ativar as demandas de prospecção geradas pelo LDR da Venda Certo, o BDR realiza a prospecção outbound, buscando novos leads e oportunidades de negócios;",
+      "Metodologia de agendamento e qualificação de reuniões: Este profissional é responsável por agendar e qualificar as reuniões comerciais de acordo com as metas e expectativas do cliente, garantindo que apenas leads qualificados sejam encaminhados para as etapas subsequentes do funil de vendas;",
+      'Gestão de CRM: O profissional gerencia o pipeline de vendas e o CRM do cliente, organizando as demandas inbound e outbound e acompanhando as etapas até a "apresentação", além de realizar o follow-up das pré-vendas, garantindo um processo de vendas eficiente e bem estruturado.',
     ],
   },
   {
-    title: "PLANO CLOSER",
+    title: "CLOSER",
     subtitleColor: "#FFD700",
     items: [
       "Tudo que engloba o plano LDR e tudo que engloba o plano BDR, além de:",
-      "1 (um) profissional closer dedicado: Este profissional, cuidador do processo comercial de ponta a ponta do cliente e coordenará os profissionais LDR e BDR e suas demandas;",
-      "Apresentação de resultados semanais ou mensais: Esta etapa é realizada entre head de CS e o closer Venda Certo para o cliente;",
-      "CRM: Este profissional é responsável por operar o pipeline de vendas/CRM do cliente com as demandas inbound e outbound geradas, até a etapa final “ganhou ou perdeu” e acompanhar os follow-ups de vendas;",
-      "Kick-off: Este profissional participa e acompanha as reuniões de Kick-off de novos clientes e faz o passão do bastão da time comercial, para o time de CS, CX ou CSM do cliente;",
-      "Eventos de networkings e reuniões presenciais: Este profissional pode participar de eventos de networking e reuniões presenciais mediante as necessidades do cliente.",
+      "Profissional Closer Dedicado: Treinado na metodologia Venda Certo, este profissional ficará à disposição do cliente para gerenciar o processo comercial de ponta a ponta, coordenando as atividades de LDR e BDR e garantindo que todas as etapas do funil sejam bem executadas;",
+      "Apresentação de Resultados: O closer realizará apresentações de resultados de forma periódica (semanal, quinzenal ou mensal) em conjunto com o Head de CS, no formato coletivo, para garantir alinhamento e transparência com o cliente;",
+      'Gestão de CRM: Este profissional é responsável por operar o pipeline de vendas e o CRM do cliente, gerenciando as demandas inbound e outbound desde a geração até a conclusão da etapa final ("ganho" ou "perdido"). Também acompanha os follow-ups de pré-vendas e vendas, assegurando o andamento contínuo e eficaz das oportunidades;',
+      "Kick-off de Novos Clientes: O closer participará das reuniões de kick-off com novos clientes, coordenando a transição entre o time comercial e as equipes de CS, CX ou CSM, garantindo uma passagem de bastão tranquila e alinhada;",
+      "Participação em Eventos e Reuniões Presenciais: Este profissional também estará disponível para participar de eventos de networking e reuniões comerciais presenciais, conforme as necessidades específicas do cliente, reforçando o relacionamento e ampliando as oportunidades de negócio.",
     ],
   },
 ];
@@ -54,7 +54,7 @@ const PlansSection: React.FC = () => {
           marginBottom: "2rem",
         }}
       >
-        NOSSOS PLANOS
+        Consulte nossos planos, mas antes entenda algumas funções!{" "}
       </Typography>
       <Box
         sx={{
@@ -79,10 +79,7 @@ const PlansSection: React.FC = () => {
               variant="h5"
               sx={{ fontWeight: "bold", textAlign: "center" }}
             >
-              <span style={{ color: colors.primaryGreen }}>PLANO</span>{" "}
-              <span style={{ color: plan.subtitleColor }}>
-                {plan.title.split(" ")[1]}
-              </span>
+              <span style={{ color: plan.subtitleColor }}>{plan.title}</span>
             </Typography>
             <Box
               component="ul"
@@ -95,8 +92,7 @@ const PlansSection: React.FC = () => {
             >
               {plan.items.map((item, itemIndex) => {
                 const isFirstItemSpecial = itemIndex === 0 && index > 0;
-                const isLastItemSpecial =
-                  index === 0 && itemIndex === plan.items.length - 1;
+
                 const [highlight, ...rest] = item.split(":");
                 const content = rest.join(":");
 
@@ -105,22 +101,15 @@ const PlansSection: React.FC = () => {
                     component="li"
                     key={itemIndex}
                     sx={{
-                      color:
-                        isFirstItemSpecial || isLastItemSpecial
-                          ? colors.primaryGreen
-                          : colors.textDark,
-                      fontWeight:
-                        isFirstItemSpecial || isLastItemSpecial
-                          ? "bold"
-                          : "normal",
-                      listStyleType:
-                        isFirstItemSpecial || isLastItemSpecial
-                          ? "none"
-                          : "disc",
+                      color: isFirstItemSpecial
+                        ? colors.primaryGreen
+                        : colors.textDark,
+                      fontWeight: isFirstItemSpecial ? "bold" : "normal",
+                      listStyleType: isFirstItemSpecial ? "none" : "disc",
                       marginTop: "0.5rem",
                     }}
                   >
-                    {isFirstItemSpecial || isLastItemSpecial ? (
+                    {isFirstItemSpecial ? (
                       <span
                         style={{
                           fontWeight: "bold",
