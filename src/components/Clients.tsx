d54486beaa48a18632e72520b5db6982a1822e39
PatrickAngrezani <pangrezani@gmail.com>
2024-11-07 21:53:23 -0300

    display clientes in a carrosel

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Slider from "react-slick";
 
 import antilhasVarejo from "../assets/antilhas-varejo.png";
 import braniva from "../assets/braniva.png";
@@ -13,28 +14,95 @@ import noAge from "../assets/no-age.png";
 import ravim from "../assets/ravim.png";
 import recrutify from "../assets/recrutify.png";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 interface Client {
+  id: number;
   name: string;
   logo: string;
 }
 
+const CustomNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", right: "-25px", color: "white" }}
+      onClick={onClick}
+    />
+  );
+};
+
+const CustomPrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", left: "-25px", color: "white" }}
+      onClick={onClick}
+    />
+  );
+};
+
 const clients: Client[] = [
-  { name: "antilhas-varejo", logo: antilhasVarejo },
-  { name: "braniva", logo: braniva },
-  { name: "cadernoVirtual", logo: cadernoVirtual },
-  { name: "chaProd", logo: chaProd },
-  { name: "grupoForest", logo: grupoForest },
-  { name: "jmfConsults", logo: jmfConsults },
-  { name: "marcasPatentes", logo: marcasPatentes },
-  { name: "nityoInfotech", logo: nityoInfotech },
-  { name: "noAge", logo: noAge },
-  { name: "ravim", logo: ravim },
-  { name: "recrutify", logo: recrutify },
+  { id: 1, name: "Antilhas Varejo", logo: antilhasVarejo },
+  { id: 2, name: "Braniva", logo: braniva },
+  { id: 3, name: "Caderno Virtual", logo: cadernoVirtual },
+  { id: 4, name: "Cha Prod", logo: chaProd },
+  { id: 5, name: "Grupo Forest", logo: grupoForest },
+  { id: 6, name: "JMF Consults", logo: jmfConsults },
+  { id: 7, name: "Marcas e Patentes", logo: marcasPatentes },
+  { id: 8, name: "Nityo Infotech", logo: nityoInfotech },
+  { id: 9, name: "No Age", logo: noAge },
+  { id: 10, name: "Ravim", logo: ravim },
+  { id: 11, name: "Recrutify", logo: recrutify },
 ];
 
-const Clients: React.FC = () => {
+const ClientCarousel = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    nextArrow: <CustomNextArrow />,
+    prevArrow: <CustomPrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <Box sx={{ padding: "4rem 0", backgroundColor: "#007f3b" }}>
+    <Box
+      sx={{
+        padding: "20px 0",
+        width: "100%",
+        margin: "auto",
+        backgroundColor: "#007f3b",
+        overflow: "hidden",
+      }}
+    >
       <Typography
         variant="h4"
         textAlign="center"
@@ -43,31 +111,24 @@ const Clients: React.FC = () => {
       >
         CONHEÇA QUEM JÁ CONFIA EM NOSSAS SOLUÇÕES
       </Typography>
-      <Grid container spacing={3} justifyContent="center">
-        {clients.map((client, index) => (
-          <Grid item xs={6} sm={4} md={3} lg={2} key={index}>
-            <Box
-              sx={{
-                padding: "1rem",
-                textAlign: "center",
-                backgroundColor: "#007f3b",
+      <Slider {...settings}>
+        {clients.map((client) => (
+          <Box key={client.id} sx={{ textAlign: "center", padding: "20px" }}>
+            <img
+              src={client.logo}
+              alt={client.name}
+              style={{
+                width: client.name === "Cha Prod" ? "40px" : "100px",
+                height: "auto",
+                objectFit: "contain",
+                margin: "0 auto",
               }}
-            >
-              <img
-                src={client.logo}
-                alt={client.name}
-                style={{
-                  width: "100%",
-                  maxHeight: "80px",
-                  objectFit: "contain",
-                }}
-              />
-            </Box>
-          </Grid>
+            />
+          </Box>
         ))}
-      </Grid>
+      </Slider>
     </Box>
   );
 };
 
-export default Clients;
+export default ClientCarousel;
